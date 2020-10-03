dcdd5cf84efe18e7456d2c618f63b30068bc2cb7
Metehan Turna <mturna@icloud.com>
2020-10-03 14:38:05 +0200

    Initial Commit

---
+++ Debuut/DebuutApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebuutApp.swift
+//  Debuut
+//
+//  Created by Metehan Turna on 03/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct DebuutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
